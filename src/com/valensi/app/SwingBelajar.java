f14453e8e602194e1a2a9f2236aff8b6af780b7a
harleith <harryoctavianus16@gmail.com>
2017-12-11 12:08:00 +0700

    final

--- src/com/valensi/app/SwingBelajar.java
+++ src/com/valensi/app/SwingBelajar.java
@@ -475,7 +475,7 @@ public class SwingBelajar extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new SwingBelajar().setVisible(true);
+                new SwingBelajar();//.setVisible(true);
             }
         });
     }
